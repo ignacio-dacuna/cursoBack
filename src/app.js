4515c88cf26b53ac3132364628e21b2f3eebab18
ignacio-dacuna <ignaciodacuna9@gmail.com>
2024-06-03 19:13:56 -0300

    terminando con algunas cosas tp2

--- src/app.js
+++ src/app.js
@@ -1,13 +1,32 @@
 import express from "express";
 import routes from "./routes/index.js";
+import __dirname from "./dirname.js";
+import handlebars from "express-handlebars";
+import { Server } from "socket.io";
+import viewsRoutes from "./routes/views.routes.js";
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.engine("handlebars", handlebars.engine()); 
+app.set("views", __dirname + "/views");
+app.set("view engine", "handlebars"); 
+app.use(express.static("public"));
 
+// Rutas de la api
 app.use("/api", routes);
 
-app.listen(8080, () => {
+// Ruta de las vistas
+app.use("/", viewsRoutes)
+
+const httpServer = app.listen(8080, () => {
     console.log("Servidor escuchando en el puerto 8080");
+});
+
+// Configuramos socket
+export const io = new Server(httpServer);
+
+io.on("connection", (socket) => {
+    console.log("Nuevo usuario Conectado");
 });
